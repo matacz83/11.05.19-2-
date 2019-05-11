d65a1ac93843e5da2ce792ed52201c2546dd2873
matacz <matacz@gmail.com>
2019-05-11 13:29:40 +0200

    MinValueSeeker + Test

---
+++ src/main/java/pl/sda/ex2/MinValueSeeker.java
@@ -0,0 +1,27 @@
+package pl.sda.ex2;
+
+import java.util.List;
+
+public class MinValueSeeker implements ValueSeeker {
+
+    @Override
+    public Integer find(List<Integer> values) {
+        validate(values);
+
+        Integer min = Integer.MAX_VALUE;
+
+        for (Integer value : values) {
+            if (min > value) {
+                min = value;
+            }
+        }
+
+        return min;
+    }
+
+    private void validate(List<Integer> values) {
+        if (values == null || values.isEmpty()) {
+            throw new EmptyListException("Lista nie może być pusta!");
+        }
+    }
+}
